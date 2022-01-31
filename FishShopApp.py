c4c6c0f749188c7e9b2df6fa200504230db2a128
Bohdan Bodakva <bodakvabohdan44@gmail.com>
2022-01-31 20:39:58 +0200

    add new version of python file

--- FishShopApp.py
+++ FishShopApp.py
@@ -1,126 +1,100 @@
-from audioop import reverse
-from hashlib import new
-from operator import attrgetter, itemgetter
-from typing import Union, List
-from xmlrpc.client import boolean
-from cv2 import sort
+from ctypes import Union
+from datetime import date
+from typing import List
 
 
-class Fish:
-
-    def __init__(self, name: str = "Unnamed fish",
-                 price_in_uah_per_kilo: float = 0, body_only: boolean = True,
-                 weight: float = 0, origin: str = "Unnamed country") -> None:
+class FishInfo:
+    def __init__(self, name, price_in_uah_per_kilo, origin, is_frozen, package_date) -> None:
         self.name = name
         self.price_in_uah_per_kilo = price_in_uah_per_kilo
-        self.body_only = body_only
-        self.weight = weight
         self.origin = origin
+        self.package_date = package_date
+        self.is_frozen = is_frozen
+
+
+class Fish (FishInfo):
+    current_weight = None
+
+    def __init__(self, name: str = "Unnamed fish", price_in_uah_per_kilo: float = 0, origin: str = "Undefined origin", is_frozen: bool = False, package_date: date = date(2022, 1, 1)) -> None:
+        super().__init__(name, price_in_uah_per_kilo, origin, is_frozen, package_date)
+
+
+class FishBox:
+    def __init__(self) -> None:
+        self.max_weight = 200
+        self.current_weight = 0
+        self.frozen_fish = []
+
+    def set_max_weight(self, max_weight: float = 100):
+        self.max_weight = max_weight
 
 
 class FishShop:
-
-    is_list_sorted = False
-
-    fish_list = []
-
-    def add_fish(self, fish: Fish):
-        max_fish_price_in_list = 0
-        for j in self.fish_list:
-            if j.price_in_uah_per_kilo > max_fish_price_in_list:
-                max_fish_price_in_list = j.price_in_uah_per_kilo
-        if max_fish_price_in_list <= fish.price_in_uah_per_kilo:
-            self.is_list_sorted = True
-        elif max_fish_price_in_list > fish.price_in_uah_per_kilo:
-            self.is_list_sorted = False
-        self.fish_list.append(fish)
-        print("Fish", fish.name, "was added to the list")
-
-    def sort_fish_names_by_price(self):
-        self.is_list_sorted = True
-        self.fish_list = sorted(
-            self.fish_list, key=lambda x: x.price_in_uah_per_kilo)
-        print("Fish list was sorted")
-
-    def print_fish_list(self):
-        if self.is_list_sorted == True:
-            print("Sorted fish list:")
-        elif self.is_list_sorted == False:
-            print("Unsorted fish list:")
-
-        for index in self.fish_list:
-            print("Fish name:", index.name,
-                  "\t\tprice:", index.price_in_uah_per_kilo,
-                  "\t\tbody only:", index.body_only,
-                  "\t\tweight:", index.weight,
-                  "kgs\t\torigin:", index.origin)
-
-    def sell_fish(self, fish_name: Fish, weight: float):
-        try:
-            if weight > fish_name.weight:
-                print("You have chosen too much weight for the fish!")
-            elif weight == fish_name.weight:
-                self.fish_list.remove(fish_name)
-                print("All the available", fish_name.name, "was sold")
-            elif weight < fish_name.weight:
-                fish_name.weight -= weight
-                print(weight, "kgs of", fish_name.name, "was sold")
-        except:
-            print("There is no such fish in fish list!")
-
-    def cast_out_old_fish(self, fsh: Fish):
-        try:
-            self.fish_list.remove(fsh)
-            print("Fish", fsh.name, "was removed from fish list")
-        except:
-            print("There is no such fish in fish list!")
-
-
-class Buyer:
     def __init__(self) -> None:
-        pass
+        self.fresh_fish = []
+        self.fish_box = FishBox()
 
-    amount_of_money = 0
+    def add_fish(self, fish: Fish, weight: float):
+        try:
+            if weight <= (self.fish_box.max_weight - self.fish_box.current_weight):
+                if fish.is_frozen:
+                    if not (fish in self.fish_box.frozen_fish):
+                        self.fish_box.frozen_fish.append(fish)
+                    self.fish_box.current_weight += weight
+                    print(weight, "kg of frozen", fish.name, "was added to fish box\t\tfillness:",
+                          self.fish_box.current_weight, "/", self.fish_box.max_weight, "kg")
+                else:
+                    if not (fish in self.fresh_fish):
+                        self.fresh_fish.append(fish)
+                    print(weight, "kg of fresh", fish.name,
+                          "was added to fish box")
+            else:
+                print("You can add only", (self.fish_box.max_weight -
+                      self.fish_box.current_weight), "kg of", fish.name, ", but not", weight, "kg")
+        except:
+            print("Fish", fish.name, "is unidentified")
 
-    number_of_bought_fish = 0
+    def sell_fish(self, fish: Fish, weight: float):
+        try:
+            if weight <= self.fish_box.current_weight:
+                if fish.is_frozen:
+                    self.fish_box.current_weight -= weight
+                    print(weight, "kg of frozen", fish.name, "was sold \t\tprofit:", weight*fish.price_in_uah_per_kilo,
+                          "hrn\t\tfillness:", self.fish_box.current_weight, "/", self.fish_box.max_weight, "kg")
+                else:
+                    print(weight, "kg of fresh", fish.name,
+                          "was added to fish box")
+            else:
+                print("You can add only", (self.fish_box.max_weight -
+                      self.fish_box.current_weight), "kg of", fish.name, ", but not", weight, "kg")
+        except:
+            print("Fish", fish.name, "is unidentified")
 
-    def buy_fish(self):
-        pass
+    def sort_fish_by_price(self):
+        new_fish_list = self.fresh_fish + self.fish_box.frozen_fish
+        for fish in sorted(new_fish_list, key=lambda x: x.price_in_uah_per_kilo):
+            print("Fish:", fish.name, "\t\tprice:",
+                  fish.price_in_uah_per_kilo, "\t\torigin:", fish.origin)
 
-    def get_current_money_balance(self):
-        pass
 
 
-class Seller:
-    def __init__(self) -> None:
-        pass
 
-    earned_money = 0
 
-    number_of_sold_fish = 0
 
-    def sell_fish(self):
-        pass
-
-    def get_earned_money():
-        pass
-
+fish_1 = Fish("Salmon", 100, "Norway", True, (2022, 1, 20))
+fish_2 = Fish("Tuna", 150, "China", True, (2022, 1, 10))
+fish_3 = Fish("Carp", 70, "Ukraine", True, (2022, 1, 5))
+fish_4 = Fish("Shark", 100, "USA", True, (2022, 1, 20))
+fish_5 = Fish("Spike", 100, "Japan", True, (2022, 1, 1))
 
 fish_shop = FishShop()
 
-fish_1 = Fish("anchovy", 400, True, 200, "Ukraine")
-fish_2 = Fish("salmon", 500, False, 400, "Turkey")
-fish_3 = Fish("carp", 1000, True, 600,  "China")
-fish_4 = Fish("bream", 250, False, 800, "Japan")
-fish_5 = Fish("pout", 10, True, 120, "South Africa")
+fish_shop.add_fish(fish_1, 10)
+fish_shop.add_fish(fish_2, 10)
+fish_shop.add_fish(fish_3, 10)
+fish_shop.add_fish(fish_4, 10)
+fish_shop.add_fish(fish_5, 10)
 
-fish_shop.add_fish(fish_1)
-fish_shop.add_fish(fish_2)
-fish_shop.add_fish(fish_3)
-fish_shop.add_fish(fish_4)
 
-fish_shop.print_fish_list()
-fish_shop.sort_fish_names_by_price()
-fish_shop.print_fish_list()
-fish_shop.add_fish(fish_5)
-fish_shop.print_fish_list()
+fish_shop.sell_fish(fish_1, 5)
+fish_shop.sort_fish_by_price()
